4b9f43f7d4de9e2f2a4e755f28409c9fe40d1607
Carlos Ferrer Hernandez <carlosferrerhernandez10@gmail.com>
2019-03-13 02:03:33 -0500

    Registrar usuarios con roles
    Cambios en columnas id

--- app/Role.php
+++ app/Role.php
@@ -5,7 +5,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
-{
+{	
+	/*
+	* Relacion muchos a muchos con modelo User
+	*/
     public function users()
 	{
     	return $this->belongsToMany(User::class)->withTimestamps();
--- database/migrations/2019_03_13_063519_create_role_user_table.php
+++ database/migrations/2019_03_13_063519_create_role_user_table.php
@@ -14,7 +14,7 @@ class CreateRoleUserTable extends Migration
     public function up()
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
 
             /*
             * Relación con tabla roles
